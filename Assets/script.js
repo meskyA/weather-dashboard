ef6669bdae0bd0b26c106bd0201565f4c5d3d3bf
meskyA <mesky2015@gmail.com>
2022-08-09 16:25:04 -0700

    restructured files and modified js and html

--- Assets/script.js
+++ Assets/script.js
@@ -5,46 +5,18 @@ var lat;
 var lon;
 
 
-function getApi() {
-//search a city 
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=2ddb14814bcbe05573b3e39270b994a1";
-
-    fetch(requestUrl)
-      .then(function (response){
-          return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-          for(var i = 0; i < userCities.length; i++) {
-              $("#cities").append()            
-          }
-      })
-      .then(function (data) {
-//put it into local storage 
-//parse out the data in order for it to be used for the lat and lon 
-        const json = JSON.parse(data);
-        console.log(json);
-       
-      })
-      .catch (function() {
-        //   catch any errors
-      });
-
-      }
-
-    //   getApi();
-// City array from local storage.
-function loadCities(){
+function loadCities() {
+//city array from local storage
     var citiesStr = localStorage.getItem("cities");
-    if(citiesStr != null) {
-        userCities = JSON.parse(citiesStr);
+    if (citiesStr !=null) {
+        userCities = JSON.parse(cities);
     }
-    // iterate through array, building divs for each city
+     // iterate through array, building divs for each city
     for(var i = 0; i < userCities.length; i++){
         $("#cities").append(buildCityDiv(userCities[i]));
     }
-
 }
+
 function buildCityDiv(city){
     var newDiv = $("<div>");
     newDiv.text(city);
@@ -53,48 +25,45 @@ function buildCityDiv(city){
     return newDiv;
 }
 
-function displayCityData(city){
+function displayCityData(city) {
     $("#city-name").text(city);
-    // showWeather(city);
+    showWeather(city);
     showForecast(city);
 }
 
-
-function showWeather(lat, lon) {
+function showWeather(city) {
     // adding another parameter after the query parameter
-    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={" + city + "&units=imperial&appid=" + APIKey;
-    //new fnction using this api to get the information for the weather forecast 
-    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon="+ lon + "&appid="+ APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + city + "&appid=" + APIKey;
 
     fetch(
     
         queryURL
    
-
+    //new fnction using this api to get the information for the weather forecast 
     ).then(function(res){
         return res.json()
-    })
-    .then(function(response) {
+    }).then(function(response) {
         console.log(response);
         
   
         // clear error text on successful call
         $("#help-text").text(""); 
-        $("#current-temp").text("Temperature: " + parseInt(response.daily[0].temp.max) + "F");
-        $("#current-humidity").text("Humidity: " + response.daily[0].humidity + "%");
-        $("#current-wind").text("Wind Speed: " + parseInt(response.daily[0].wind.speed) + " MPH");
+        $("#current-temp").text("Temperature: " + parseInt(response.main.temp) + "F");
+        $("#current-humidity").text("Humidity: " + response.main.humidity + "%");
+        $("#current-wind").text("Wind Speed: " + parseInt(response.wind.speed) + " MPH");
 
-        var date = moment.unix(response.dt);
-        var dateStr = date.format("M/DD/YYYY");
-        $("current-date").text(dateStr);
+        var TimeStamp = (response.dt * 1000);
+        var date = new Date(TimeStamp);
+        var dateStr = ' (' + date.toLocaleDateString() + ')';
+        $("#current-date").text(dateStr);
 
         var icon = response.weather[0].icon;
         var iconURL = "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
         $("#current-icon").attr("src", iconURL);
         $("#current-icon").attr("alt", "weather icon");
 
-        var bgURL = getBgURL(icon);
-        $("#current-bg").attr("src", bgURL);
+        // var bgURL = getBgURL(icon);
+        // $("#current-bg").attr("src", bgURL);
         $("#current-bg").attr("alt", "weather backgraound image");
 
         var lon = response.coord.lon;
@@ -102,13 +71,12 @@ function showWeather(lat, lon) {
 
         queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
 
-        fetch({
-            url: queryURL,
-            method: "GET",
-            error: function(){
-                console.log("UV Index call failed");               
-            }
-        }).then(function(response){
+        fetch(queryURL
+      ).then(function(res){
+          return res.json()
+      }
+      ).then(function (response){
+
             var uvIndex = response.value;
             var uvColor = "";
             if (uvIndex < 3){uvColor = "green";}
@@ -119,10 +87,18 @@ function showWeather(lat, lon) {
             $("#uv-label").text("UV Index: ");
             $("#current-uv").text(uvIndex);
             $("#current-uv").attr("style", "background-color: " + uvColor);
+        }).catch((error) => {
+            console.log("UV Index call failed");
         });
     });
 }
 function showForecast(city) {
+
+    var currentDate = new Date();
+    var ForacstDate;
+    var DateFiff = 0;
+    var ForacstDay = 0;
+
     queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
 
     fetch(
@@ -133,9 +109,41 @@ function showForecast(city) {
         return res.json()
     })
     .then(function(response){
-        console.log(response);
-        showWeather(response.city.coord.lat, response.city.coord.lon);
-    });
+      
+         
+            var forcastList = response.list;
+            var forcastListLength = forcastList.length;
+            var item;
+
+            for(var i = 0; i < forcastListLength; i++){
+                item = forcastList[i];
+            // return to break the forecast loop   
+            if (ForacstDay >= 6)
+            return;
+
+            ForecastDate = new Date(item.dt * 1000);
+            DateFiff = Math.round((ForacstDate.getTime() - currentDate.getTime()) / (3600*1000));
+
+            if (DateFiff >= 21) {
+                currentDate = ForecastDate;
+
+                var icon =item.weather [0].icon;
+                var iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+                $("#forcast-icon-" + ForacstDay).attr("src", iconURL);
+
+                var dateStr = ForacstDate.toLocaleDateString();
+                $("#forcast-date-" + ForacstDay).text(dateStr);
+
+                $("#forcast-temp-" + ForacstDay).text('Temp: '+item.main.temp + ' °F');
+                $("#forcase-wind-" + ForacstDay).text('Wind: '+item.wind.speed + ' MPH');
+                $("#forcase-humidity-" + ForacstDay).text('Humidity: '+item.main.humidity + ' %');
+
+                ForacstDay++;
+
+            }
+    }
+});
+
 }
 
 function addCity(city) {
@@ -146,7 +154,6 @@ function addCity(city) {
             userCities.splice(i, 1);
         }
     }
-
     // add city to front of array
     userCities.splice(0, 0, city);
 
@@ -160,6 +167,7 @@ function addCity(city) {
 function storeCities(){
     localStorage.setItem("cities", JSON.stringify(userCities));
 }
+loadCities();
 
     $("#city-submit").on("click", function(){
         event.preventDefault();
@@ -173,6 +181,3 @@ function storeCities(){
 
         }
     });
-
- 
-
